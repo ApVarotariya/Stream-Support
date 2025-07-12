5fcb38554473e0ffcd0adbbe68625a0308c6b728
ApVarotariya <apvarotariya17@gmail.com>
2025-07-12 15:29:53 +0530

    change in origin

--- lib/getInfo.ts
+++ lib/getInfo.ts
@@ -8,12 +8,12 @@ export default async function getInfo(id: string) {
     const response = await axios.get(`${playerUrl}/play/${id}`, {
       headers: {
         Accept:
-          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "en-US,en;q=0.9",
         DNT: "1",
         "sec-ch-ua":
-          '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+        '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
         "sec-ch-ua-mobile": "?0",
         "sec-ch-ua-platform": '"Windows"',
         "Sec-Fetch-Dest": "document",
@@ -21,13 +21,14 @@ export default async function getInfo(id: string) {
         "Sec-Fetch-Site": "none",
         "Sec-Fetch-User": "?1",
         "Upgrade-Insecure-Requests": "1",
-        Origin: "https://allmovieland.fun/",
+        Origin: "https://allmovieland.ac/",
         Referer: "https://google.com",
         "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0",
       },
     });
     const $ = cheerio.load(response.data);
+    console.log(cheerio, $);
     // get last script tag
     const script = $("script").last().html()!;
     if (!script) {
